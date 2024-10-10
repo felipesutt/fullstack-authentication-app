175a43653d52535bd6dea511db7f865b6d3e0a5c
felipe sutt <felipesa232@gmail.com>
2024-10-10 18:37:54 -0300

    Implemented fingerprint authentication

--- backend/server/views.py
+++ backend/server/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+import os
+import cv2
+
 from .models import EnvironmentalData
 
 global user_access_level
@@ -26,8 +29,68 @@ def login_user(request):
 @csrf_exempt
 def digital_authentication(request):
     global user_access_level
-    if request.method == 'GET':
-        return JsonResponse({'access_level': user_access_level})
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        if data.get('type') == 'correct':
+            fingerprint_img = cv2.imread("server/authentication/fingerprint_detection/3__M_Left_index_finger.BMP")  # Lendo a imagem principal correta
+        elif data.get('type') == 'wrong':
+            fingerprint_img = cv2.imread("server/authentication/fingerprint_detection/11__M_Right_ring_finger.BMP") # Lendo a imagem principal errada
+
+        fingerprint_img = cv2.resize(fingerprint_img, None, fx=2.5, fy=2.5)  # Resize da imagem
+
+        sift_algorithm = cv2.SIFT.create()  # Criação do objeto do algoritmo SIFT(Scale Invariant Feature Transform)
+        kp1, descriptor_1 = sift_algorithm.detectAndCompute(fingerprint_img, None)  # Rodar o algoritmo SIFT na imagem principal, kp1 é uma tupla de objetos KEYPOINT do opencv, descriptor é um array
+
+        files_path = os.listdir("server/authentication/fingerprint_detection/SOCOFing/Real")  # Path de cada imagem a ser comparada
+
+        best_score = 0  # Inicializar o melhor score
+        best_filename = None  # Inicializar o nome do melhor arquivo
+        best_image = None  # Inicializar a melhor imagem
+        best_kp2 = None  # Inicializar os keypoints da melhor imagem
+        best_match_points = None  # Inicializar os pontos de correspondência
+
+        for file in files_path: # Iterar cada imagem, cortei a lista pra ser mais rapido
+            print(file)  # Printar o nome do arquivo que está sendo acessado
+
+            to_compare_fingerprint = cv2.imread("server/authentication/fingerprint_detection/SOCOFing/Real/" + file)
+            to_compare_fingerprint = cv2.resize(to_compare_fingerprint, None, fx=2.5, fy=2.5)  # Resize da imagem
+
+            kp2, descriptor_2 = sift_algorithm.detectAndCompute(to_compare_fingerprint, None)  # Rodar o algoritmo SIFT na imagem a comparar
+
+            # Comparar descritores usando Flann Matcher
+            matches = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10}, {}).knnMatch(descriptor_1, descriptor_2, k=2) # Retorna uma tupla de objetos Dmatch do opencv, NÃO SEI COMO FUNCIONA DIREITO
+
+            match_points = []
+            for p, q in matches: # Encontrar pontos correspondentes, NÃO SEI COMO FUNCIONA DIREITO TAMBÉM
+                if p.distance < 0.1 * q.distance:
+                    match_points.append(p)
+
+            # Calcular a quantidade de keypoints
+            keypoints = min(len(kp1), len(kp2))
+
+            # Calcular o score
+            score = len(match_points) / keypoints * 100 if keypoints > 0 else 0
+
+            print(f"Score: {score:.2f}, Matches: {len(match_points)}, Keypoints: {keypoints}")
+
+            # Atualizar se o score atual for melhor
+            if score > best_score:
+                print("Melhor correspondência encontrada!")
+                best_score = score
+                best_filename = file
+                best_image = to_compare_fingerprint
+                best_kp2 = kp2
+                best_match_points = match_points
+
+        # Exibir o resultado da melhor correspondência
+        if best_image is not None:
+            print("BEST MATCH: " + best_filename)
+            print("SCORE: " + str(best_score))
+        else:
+            print("Nenhuma correspondência encontrada.")
+
+        return JsonResponse({'access_level': user_access_level, 'score': best_score})
     
 @csrf_exempt
 def facial_authentication(request):
--- frontend/src/renderer/src/components/Fingerprint.jsx
+++ frontend/src/renderer/src/components/Fingerprint.jsx
@@ -6,16 +6,24 @@ import { useNavigate } from "react-router-dom";
 export default function Fingerprint({handleModalVisibility, showFacialModal}) {
   const navigate = useNavigate(); // Hook para navegação
 
-  async function handleCorrectFinger(){
-    axios.get('http://localhost:8000/server/fingerprint')
+  async function handleFinger(type) {
+    const data = {
+      'type': type
+    };
+
+    axios.post('http://localhost:8000/server/fingerprint', data)
     .then(
       (response) => {
-        if (response.data.access_level == 3){
-          console.log(response.data.access_level)
+        if (response.data.access_level == 3 && response.data.score >= 90.0){
+          console.log(response.data)
           showFacialModal()
         }
-        else{
+        else if (response.data.access_level != 3 && response.data.score >= 90.0){
+          console.log(response.data)
           navigate('/home')
+        }
+        else if (response.data.score < 90.0){
+          console.log("Digital incorreta!")
         }
       }
     )
@@ -26,10 +34,6 @@ export default function Fingerprint({handleModalVisibility, showFacialModal}) {
     )
   }
 
-  async function handleWrongFinger(){
-    console.log('teste')
-  }
-
   return (
     <div className="h-full w-full bg-gray-700 bg-opacity-75 absolute left-0 top-0 flex items-center justify-center">
       <div className="bg-neutral-500 rounded-md border-neutral-700 border-4 bg-opacity-85 p-3 flex flex-col items-center gap-3 w-80">
@@ -38,8 +42,8 @@ export default function Fingerprint({handleModalVisibility, showFacialModal}) {
           <p className="flex justify-center font-semibold text-lg">Aproxime o dedo do sensor</p>
         </div>
         <div className="flex gap-4">
-          <button onClick={handleWrongFinger} className="bg-orange-700 hover:bg-orange-600 text-black font-semibold px-4 py-2 rounded-lg">Dedo errado</button>
-          <button onClick={handleCorrectFinger} className="bg-green-900 hover:bg-green-700 text-black font-semibold px-4 py-2 rounded-lg">Dedo Correto</button>
+          <button onClick={()=>{handleFinger('wrong')}} className="bg-orange-700 hover:bg-orange-600 text-black font-semibold px-4 py-2 rounded-lg">Dedo errado</button>
+          <button onClick={()=>{handleFinger('correct')}} className="bg-green-900 hover:bg-green-700 text-black font-semibold px-4 py-2 rounded-lg">Dedo Correto</button>
         </div>
         <button className="bg-red-500 hover:bg-red-400 text-black font-semibold px-4 py-2 rounded-lg" onClick={handleModalVisibility}>Fechar</button>
       </div>
